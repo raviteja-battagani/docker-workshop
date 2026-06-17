3b492d9a136e360955d636ca14c6bfe20fdbdfd7
raviteja-battagani <42055617+raviteja-battagani@users.noreply.github.com>
2026-06-17 08:06:29 +0000

    Finished ingestion script

---
+++ pipeline.py
@@ -0,0 +1,10 @@
+import sys
+month = sys.argv[1]
+print('arguments passed:', sys.argv )
+print(f"Running pipeline for month: {month}")
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
